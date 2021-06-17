6a610c636261b0a7deedd610a7af8e9c5dfbd959
Archimedes Trajano <developer@trajano.net>
2021-06-17 13:23:41 -0400

    Proof of https://github.com/CareLuLu/react-native-web-jsonschema-form/issues/254

---
+++ missing.d.ts
@@ -0,0 +1,2 @@
+declare module 'react-native-web-ui-components';
+declare module 'react-native-web-jsonschema-form';
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,17 +1,59 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import captureMomentForm from '../capture-moment-form.json';
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import type { StackNavigationProp } from "@react-navigation/stack";
+import * as React from "react";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { UIProvider } from "react-native-web-ui-components";
+import captureMomentForm from "../capture-moment-form.json";
+import Form from "react-native-web-jsonschema-form";
+import { Text, View } from "../components/Themed";
 
-export default function TabOneScreen() {
+export default function TabOneScreen({
+  navigation,
+}: {
+  navigation: StackNavigationProp<{}>;
+}) {
+  const [formData, setFormData] = useState(captureMomentForm.form.form_data);
 
+  // Sample onChange
+  const onChangeSample = (event) =>
+    setFormData({
+      ...formData,
+      [event.params.name]: event.params.value,
+    });
+
+  // intentionally does not change any state
+  const onChangeLogging = (event) => {
+    console.log(event);
+  };
+
+  const theme = {
+    input: {
+      focused: StyleSheet.create({
+        border: {
+          borderColor: "yellow",
+        },
+      }),
+    },
+  };
+  const ThemeWrapper = ({ children }: { children: JSX.Element }) => {
+    // this part differs from the example in that it uses React Navigation
+    return (
+      <UIProvider theme={theme} history={navigation}>
+        {children}
+      </UIProvider>
+    );
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <Text style={styles.title}>{JSON.stringify(captureMomentForm)}</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <ThemeWrapper>
+        <Form
+          formData={formData}
+          schema={captureMomentForm.form.json_schema}
+          uiSchema={captureMomentForm.form.ui_schema}
+          onChange={onChangeLogging}
+        />
+      </ThemeWrapper>
     </View>
   );
 }
@@ -19,16 +61,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
